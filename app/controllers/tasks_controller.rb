4a3a147fd1330a9e9ff7e8dc62c50ceb76b76336
hayato0904 <sakigi94@gmail.com>
2021-07-30 13:12:30 +0000

    ステータスメッセージ登録

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -64,6 +64,6 @@ class TasksController < ApplicationController
     end
 
     def task_params
-      params.require(:task).permit(:title, :content, :expired_at)
+      params.require(:task).permit(:title, :content, :expired_at, :status)
     end
   end
