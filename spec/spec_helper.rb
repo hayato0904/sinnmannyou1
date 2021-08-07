da8f0422dc45c0e57e9d629fd2cbe47d2558e1d8
hayato <sakigi94@gmail.com>
2021-08-07 10:13:48 +0000

    げ

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,7 @@ RSpec.configure do |config|
         options.add_argument('no-sandbox')
       end
     end
-  end
+  endd
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
