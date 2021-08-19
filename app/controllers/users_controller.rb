670ea98f318fb5058e2aeb819416e66541bf1cfe
hayato <sakigi94@gmail.com>
2021-08-19 14:21:38 +0000

    新規登録した際に、ログインも同時に行う。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
       # _pathっているっけ？ 後で確認しなくきゃ
     else
