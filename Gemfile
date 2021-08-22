eefde595a8130dd88804eb80fe77acc31f91aa4f
hayato <sakigi94@gmail.com>
2021-08-22 07:34:21 +0000

    管理者権限でユーザー編集機能追加

--- Gemfile
+++ Gemfile
@@ -48,6 +48,8 @@ group :development, :test do
   gem 'pry-rails'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'meta_request'
+
 end
 
 group :development do
---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,52 @@
+class Admin::UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+  # 上記ArgumentErrorが発生した。
+  # login?required が定義されていないとケラー分が出ている。
+  # 仮説：login_requiredが存在しているかを確認すべきである。
+  # そもそも skip_before_actionの定義をちゃんと理解すべきである。
+  # また記載する場所の確認も必要だ。
+    def index
+      @users = User.all.page(params[:page]).per(6)
+    end
+
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to admin_users_path
+        # _pathっているっけ？ 後で確認しなくきゃ
+      else
+        render :new
+      end
+    end
+  
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def edit
+      @user = User.find(params[:id])
+      # NoMethodError in Admin::Users#edit
+      # editの内容を削除すると、上記のエラーが生じてしまうのでだめだ。でもtaskはなんでeditの中身がないのに編集できるんだ？
+    end
+
+    def update
+      # そもそもなんでbinding.pryで止まらないんだ。
+      # 処理が来てない？ いやでも、
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to admin_users_path
+      else
+        render :edit
+    end
+  end
+  
+      def user_params
+        params.require(:user).permit(:name, :email, :password,
+          :password_confirmation)
+        end
+  end
+  
