a3a17877838efe2e9d9f7c11409bd9d1150e91b3
hayato <sakigi94@gmail.com>
2021-08-27 22:17:18 +0000

    herokuにデプロイした時のエラー修正完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# 2.times do |n|
-#   name = Faker::Games::Pokemon.name
-#   email = Faker::Internet.email
-#   password = "password"
-#   User.create!(
-#     name: name,
-#     email: email,
-#     password: password,
-#   )
-# end
+This file should contain all the record creation needed to seed the database with its default values.
+The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
-# User.create!(name:  "管理者",
-#   email: "admin1@example.jp",
-#   password: "11111111",
-#   password_confirmation: "11111111",
-#   admin: true)
+Examples:
+
+  movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+  )
+end
+
+User.create!(name:  "管理者",
+  email: "admin1@example.jp",
+  password: "11111111",
+  password_confirmation: "11111111",
+  admin: true)
 
   Label.create([
     { name: 'Ruby' },
